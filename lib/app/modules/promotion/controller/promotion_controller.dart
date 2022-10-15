92d37ca036668f6375786d389d415acd0a8e3d95
hoangduongle <lehhoangduong@gmail.com>
2022-10-15 14:32:31 +0700

    add core api promotion

--- lib/app/modules/promotion/controller/promotion_controller.dart
+++ lib/app/modules/promotion/controller/promotion_controller.dart
@@ -1,3 +1,29 @@
+import 'package:get/get.dart';
 import 'package:smarttv_app/app/core/base/base_controller.dart';
+import 'package:smarttv_app/app/core/model/promotion_content.dart';
+import 'package:smarttv_app/app/data/repository/repository.dart';
 
-class PromotionController extends BaseController {}
+class PromotionController extends BaseController {
+  final Repository _repository = Get.find(tag: (Repository).toString());
+  Rx<List<PromotionContent>> promotionList = Rx<List<PromotionContent>>([]);
+
+  @override
+  void onInit() {
+    fetchPromotion();
+    super.onInit();
+  }
+
+  Future<void> fetchPromotion() async {
+    var overview = _repository.getListPromotion();
+    List<PromotionContent> result = [];
+
+    await callDataService(
+      overview,
+      onSuccess: (List<PromotionContent> response) {
+        result = response;
+      },
+      onError: ((dioError) {}),
+    );
+    promotionList(result);
+  }
+}
---
+++ lib/app/modules/promotion/view/promotion_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:smarttv_app/app/modules/promotion/controller/promotion_controller.dart';
+
+class PromotionScreen extends GetView<PromotionController> {
+  const PromotionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
